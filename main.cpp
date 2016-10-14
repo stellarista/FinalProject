a98856f016167cb31ccbef646da5f72115463b03
so bi <so bi>
2016-10-14 08:49:30 +0700

    add existing file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void quiestionOne();
+
+int main()
+{
+    string name;
+    cout << "Write your name here: " << endl;
+    cin >> name;
+    cout << "Hello " << name << "! Let's get started!";
+
+    return 0;
+}
+
