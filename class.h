8122dac45cf23932b21c12ad6d00d0a6ce8b3092
so bi <so bi>
2016-11-06 19:05:15 +0700

    Final Project finished^.^

--- class.h
+++ class.h
@@ -1,37 +1,42 @@
 #ifndef CLASS_H_INCLUDED
 #define CLASS_H_INCLUDED
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <cmath>
 
 using namespace std;
 
 class Answer
 {
-private:
+    private:
     char answer;
     int mark = 0;
-public:
+
+    public:
+    /*To call the class*/
     Answer()
     {
         this->answer=answer;
         this->mark=mark;
     }
+    /*Initialized answer:char*/
     void setAnswer(char answer)
     {
+        /*Pointing that the answer in the private class is also the answer in this function*/
         this->answer=answer;
-        if (answer == 'a'){
+
+        /*Answer validation for the input*/
+        /*Each answer has a score or mark and will be calculated*/
+        if (answer == 'a' || answer == 'A'){
             mark += 3;
-        } else if (answer == 'b'){
+        } else if (answer == 'b' || answer == 'B'){
             mark += 2;
-        } else if (answer == 'c'){
+        } else if (answer == 'c' || answer == 'C'){
             mark += 1;
-        } else if (answer == 'd'){
-            mark += 0;
-        } while (answer != 'a' && answer != 'b' && answer != 'c' && answer != 'd')
+
+            /*Use while loop so the program will keep asking the user to put the right input if the user always put the wrong input*/
+        } while (answer != 'a' && answer != 'A' && answer != 'b' && answer != 'B' && answer != 'c' && answer != 'C')
 			{
-				cout << "Invalid answer. Please enter a valid selection, either a, b, c, or d.\n";
+			    /*Display the warning and ask the user to enter a valid input*/
+				cout << "Invalid answer.\n"<<"Please enter a valid selection, either a, b, or c.\n";
 				cin >> answer;
 			}
 
@@ -39,7 +44,7 @@ public:
 
     char getAnswer()
     {
-        return answer;
+        return answer; //return answer
     }
     void setMark(int mark)
     {
@@ -47,27 +52,61 @@ public:
     }
     int getMark()
     {
-        return mark;
+        return mark; //return mark
     }
 
-    void print()
+    /*Display the result with validations*/
+    void printResult()
     {
-        if (mark >= 0 & 5 >= mark)
-        {   cout<< "Hasil:\n\n";
-            cout << "Kamu orang yang naif, dan  gampang terbawa angin. Agak plin plan tapi memiliki penasaran yang tinggi dalam segala hal, dan naif yang kamu punyai membuatmu takut untuk berbohong. Cobalah lebih berani menentukan sikap, agar tidak gampang di manfaatkan orang.";
+        /*The sum of the mark will decided which result the user get*/
+        if (mark >= 13 & 17 >= mark)
+        {   cout<< "Result:\n\n";
+            cout<< "You think yourself as an inferior.\n\n";
+            cout<< "No one is born inferior. Everyone is superior and a winner.\n"
+                << "You're too shy to express yourself.\n"
+                << "You're afraid people might avoid you if you're wrong\n"
+                << "You should try to express yourself more because\n"
+                << "although not everyone has the same thoughts as you,\n"
+                << "you will find some people who will be with you.\n"
+                << "You just need to be a little more confident~\n";
         }
 
-        if (mark >= 6 & 10 >= mark)
-        {   cout<< "Hasil:\n\n";
-            cout << "Kamu agak bisa menentukan sikap walau terkadang kamu suka ikut-ikutan jadi penghasut. Kamu pintar yang membuat orang lain menyukaimu. Tapi agar kamu lebih sempurna cobalah lebih memendam keinginanmu untuk tidak ikut menjadi penghasut.";
+        if (mark >= 18 & 22 >= mark)
+        {   cout<< "Result:\n\n";
+            cout<< "You're tend to be a shy person\n\n";
+            cout<< "You're a person who don't want to take risk\n"
+                << "because you're afraid to be blamed if you make mistake.\n"
+                << "Everyone has a potential including you.\n"
+                << "Try to overcome it so your potential can be unleashed.\n";
         }
 
-        if (mark >= 11 & mark >= 15)
-        {   cout<< "Hasil:\n\n";
-            cout << "Kamu orangnya sangat bersemangat, tidak gampang menyerah, tapi agak sedikit egois juga otoriter. Cobalah sesekali dengarkan apa kata orang lain, karena belum tentu orang lain bisa menerima keinginanmu.";
+        if (mark >= 23 & 28 >= mark)
+        {   cout<< "Result:\n\n";
+            cout<< "You're a confident person~\n\n";
+            cout<< "You have enough confidence and placed yourself well.\n"
+                << "You know well how to interact with others.\n"
+                << "Positioning yourself if important to you.\n";
+        }
+
+        if (mark >= 29 & 34 >= mark)
+        {   cout<< "Result:\n\n";
+            cout<< "You're a really confident person~\n\n";
+            cout<< "You have a huge confident.\n"
+                << "You're a person people can rely on and can express yourself well.\n"
+                << "Being able to learn new things is you most powerful strength.\n";
+        }
+
+        if (mark >= 35 & 39 >= mark)
+        {   cout<< "Result:\n\n";
+            cout<< "You're a narcissist\n\n";
+            cout<< "Be careful!\n"
+                << "It's good to have confidence but don't be too much!\n"
+                << "Your overflowing confidence can sometimes make people uncomfortable.\n"
+                << "Try to control it.\n";
         }
     }
 };
 
 
 #endif // CLASS_H_INCLUDED
+
--- main.cpp
+++ main.cpp
@@ -1,66 +1,155 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <cmath>
-
+#include <iostream> // input output stream > to run cin/cout
+#include <cstdlib> // to run system function
 #include "class.h"
 
 using namespace std;
 
 int main()
 {
-
+    /*Declare the string input*/
     string name;
+
+    /*The front page display*/
     cout << "----------------------------------------" << endl;
-    cout << "           Personality Test"<<endl;
+    cout << "|                                      |" << endl;
+    cout << "|          Personality Test            |" << endl;
+    cout << "|           (@PsikologiID)             |" << endl;
+    cout << "|                                      |" << endl;
     cout << "----------------------------------------" << endl;
-    cout << "Write your name here: " << endl;
+    cout << "Write your name: ";
     cin >> name;
+
+    /*Clear the screen after the input to move to the next display*/
     system("CLS");
 
-    cout << "Hello " << name << "!\n";
+    /*Display greetings and instructions*/
+    cout << "                 Hello " << name << "!\n\n";
+    cout << "Welcome to Stella's Personality Test Program!\n\n";
+    cout << "This test will decided what kind of person you're in three categories: "<<endl;
+    cout << "- a not confident person\n";
+    cout << "- a confident person\n";
+    cout << "- a narcissist\n";
+
     cout << "Please choose the answer that suits you the most!\n";
-    cout << "choose bu typing a, b, c, or d~"<< endl;
-    system("pause");
+    cout << "choose by typing a, b, or c~"<< endl<<endl;
+
+    system("pause"); //To let the user to press any key to continue
     system("CLS");
 
+    /*Declare the char input*/
     char answer;
 
+    /*Declare the class*/
     Answer input;
 
-    cout << "What's your favorite among these colors?"<<endl;
-    cout << "a. Red  b. Blue  c. Black  d. White"<<endl;
+    /*Display the question*/
+    cout << "1) In your opinion, what kind of person are you?"<<endl;
+    cout << "A. I am a special person\n"<<
+            "B. I am like the others\n"<<
+            "C. I am not better than the others\n";
+    cin >> answer;
+    input.setAnswer(answer); //Connect the input with the validation in the void setAnswer(char answer) function
+    system("CLS");
+
+    cout << "2) Can you influence the others?"<<endl;
+    cout << "A. I have the natural ability to influence the others\n"<<
+            "B. I can influence the others if I want\n"<<
+            "C. I am easily influenced by the others\n";
     cin >> answer;
     input.setAnswer(answer);
     system("CLS");
 
-    cout << "What's your least favorite song genres among of these?"<<endl;
-    cout << "a. Rock  b. Jazz  c. Pop  d. Dangdut"<<endl;
+    cout << "3) Critics can make me really embarrassed."<<endl;
+    cout << "A. Never\n"<<
+            "B. Not really\n"<<
+            "C. Yes\n";
     cin >> answer;
     input.setAnswer(answer);
     system("CLS");
 
-    cout << "Who's your favorite among these Indonesian president?"<<endl;
-    cout << "a. Soekarno  b. Habibie  c. Soeharto  d. SBY"<<endl;
+    cout << "4) In my opinion, I am a reliable leader."<<endl;
+    cout << "A. Yes\n"<<
+            "B. Maybe\n"<<
+            "C. No\n";
     cin >> answer;
     input.setAnswer(answer);
     system("CLS");
 
-    cout << "Which fruit is the most delicious for you?"<<endl;
-    cout << "a. Strawberry  b. Grape  c. Apple  d. Orange"<<endl;
+    cout << "5) In a group, you usually positioned yourself as?"<<endl;
+    cout << "A. I always want to be the leader\n"<<
+            "B. My friends usually recommend me to be the leader\n"<<
+            "C. I don't mind just being a follower\n";
     cin >> answer;
     input.setAnswer(answer);
     system("CLS");
 
-    cout << "What's your most favorite place?"<<endl;
-    cout << "a. House  b. Cafe  c. Mall  d. Park"<<endl;
+    cout << "6) In my opinion, I am a reliable leader."<<endl;
+    cout << "A. I can do many things well\n"<<
+            "B. I accept and believe in myself\n"<<
+            "C. I often think I am a foolish person\n";
     cin >> answer;
     input.setAnswer(answer);
     system("CLS");
 
-    cout<<endl;
+    cout << "7) How do you interact and socialize with people?"<<endl;
+    cout << "A. I always or often started a conversation\n"<<
+            "B. I feel comfortable making acquaintances\n"<<
+            "C. I tend to have a hard time to socialize or making new friends\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
 
-    input.print();
+    cout << "8) In a meeting, what you usually do?"<<endl;
+    cout << "A. I usually lead the meeting\n"<<
+            "B. I am more to listening\n"<<
+            "C. I am too shy to give my opinion or idea\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+    cout << "9) You're in front of the audiences, what's the thing that usually happened?"<<endl;
+    cout << "A. I am confident. Even sometimes excessive\n"<<
+            "B. I am confident but still a bit nervous\n"<<
+            "C. I am too shy to give my opinion or idea\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+    cout << "10) In your opinion, what obstacles usually encountered in a friendship?"<<endl;
+    cout << "A. My friend does not understand what I am sain\n"<<
+            "B. Miscommunication\n"<<
+            "C. I feel like my friends only pay attention to my weakness\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+    cout << "11) What usually the reason behind a fight?"<<endl;
+    cout << "A. Unappreciated opinion\n"<<
+            "B. Miscommunication\n"<<
+            "C. Rudeness\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+    cout << "12) What kind of place that you like?"<<endl;
+    cout << "A. A place of people\n"<<
+            "B. A comfortable place\n"<<
+            "C. A quiet place\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+    cout << "13) What is your reason if you want to reject a task?"<<endl;
+    cout << "A. The task is too easy\n"<<
+            "B. You don't have the capabilities\n"<<
+            "C. Feel like you will fail\n";
+    cin >> answer;
+    input.setAnswer(answer);
+    system("CLS");
+
+
+    /*Display the result based on the validation in void printAnswer() function*/
+    input.printResult();
     return 0;
 }
 
